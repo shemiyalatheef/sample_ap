631a661fcace9482755e9dde25b309bb2e110fa4
shemi <shemi.ideology@gmail.com>
2014-08-08 10:13:31 +0530

    sign in and sign out tests completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ SampleAp::Application.routes.draw do
   get "users/new"
   
  resources :users
+ resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
